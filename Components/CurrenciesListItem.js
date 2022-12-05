e227de1598a2e62a9a51b5588b1ee922761c89d2
Josep <joseptnl@gmail.com>
2022-12-05 18:07:04 +0100

    Finished exchange rates list

--- Components/CurrenciesListItem.js
+++ Components/CurrenciesListItem.js
@@ -9,18 +9,16 @@ import CurrencyRateSelector from "./CurrencyRateSelector.js";
 import React from "react";
 
 CurrenciesListItem = ({ currency, currenciesData, updateExpansion, expanded }) => {
-    let counter
 
-    calculate = (currencyToConvert) => {
-        if (currenciesData[0].id == currencyToConvert.id) counter = false
-        else counter = false
-        return currency.id != currencyToConvert.id ? currency.rates * currencyToConvert.rates : 1
+    calculate = (baseCurrencyId, currencySelectedId, baseCurrencyRates, currencySelectedRates) => {
+        return baseCurrencyId != currencySelectedId ? baseCurrencyRates * currencySelectedRates : 1
     }
 
     const [output, setOutput] = React.useState(calculate(currenciesData[0]))
 
     passCurrency = newCurrency => {
-        setOutput(calculate(newCurrency))
+        let value = calculate(currency.id, newCurrency.id, currency.rates, newCurrency.rates)
+        setOutput(value)
     }
 
     return (
